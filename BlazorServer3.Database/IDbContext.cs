e3d7796d023079f5e3d0b0c5d6ef1342994c944f
Graham Fearn <graham@fearnmarketingservices.com>
2020-06-10 23:18:33 +0100

    Add project files.

---
+++ BlazorServer3.Database/IDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace BlazorServer3.Database
+{
+    public interface IDbContext : IDisposable
+    {
+        void RecreateDatabase();
+
+        DbSet<Category> Categories { get; set; }
+
+        DbSet<Product> Products { get; set; }
+
+        int SaveChanges();
+    }
+}
